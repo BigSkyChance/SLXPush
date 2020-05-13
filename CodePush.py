4d34205433e0a7b6a765e9899a21a64f8e76e3e1
Chancellor Griffin <motochance74@gmail.com>
2020-05-13 00:38:03 -0500

    Create CodePush.py

---
+++ CodePush.py
@@ -0,0 +1,18 @@
+import netmiko
+import time
+
+IPlist = []
+for ip in open('iplist.txt', 'r').readlines():
+  IPlist.append(ip.strip())
+  
+def main(IPlist):
+  for IP in IPlist:
+    VCPSLX = netmiko.ConnectHandler(ip = IP, device_type = 'extreme_slx', username = 'username', password = 'password')
+    print(VCPSLX.send_command('show version'))
+    time.sleep(2)
+    VCPSLX.send.command('firmware download REDACTED LINE') # Code download begins on first switch
+    VCPSLX.disconnect() # disconnects from switch
+    time.sleep(300) # Spaces out downloads as to not overload jump server
+    
+main(IPlist)
+
